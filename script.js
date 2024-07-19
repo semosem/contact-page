220c27e8149cf3163926ed723b43880a6489f5cb
semosem <sem.gebresilassie@gmail.com>
2024-07-19 15:14:14 +0300

    feat add animations

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const resumeContent = [
-  "Team player with versatile skill set for diverse software engineering projects, quick at adapting to new challenges and technologies.",
+  "Extensive expertise in diverse, team-oriented software engineering projects",
   "In-depth understanding of ReactJS, VueJS, NodeJS, and Angular, with a track record of developing robust applications in each.",
-  "My Git commits are precise and well-documented, ensuring easy collaboration and maintenance.",
-  "Proficient in debugging, able to identify and resolve complex issues quickly and effectively.",
+  "Complemeted for my git commits to be precise and well-documented, ensuring easy collaboration and maintenance.",
+  "Expert at identifying and fixing bugs, with a proactive approach to problem-solving.",
   "Strong grasp of Component-based UIs, HTML DOM tree, render tree, and critical rendering path, ensuring optimal performance and user experience.",
   "8+ years of proven experience in building TypeScript and JavaScript web services and web development. Successfully led projects that improved system efficiency by 30%.",
   "Fast and efficient coding skills, consistently delivering high-quality work ahead of deadlines.",
-  "Expert at identifying and fixing bugs, with a proactive approach to problem-solving.",
   "My code is optimized for performance, resulting in fast and reliable applications.",
 ];
 
